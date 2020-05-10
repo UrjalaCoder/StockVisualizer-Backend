2cfcf7e9ca74015a8aac43df3c7ba05c10acb271
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2020-05-10 20:48:50 +0300

    Templates

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 # Create the app instance
 app = Flask(__name__)
 
-
+# Basic index routing
+@app.route("/index")
 @app.route("/")
 def main():
-    return "Works!"
+    return render_template("index.html")
 
