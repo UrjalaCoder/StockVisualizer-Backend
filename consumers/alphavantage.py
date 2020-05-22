7349c82425ae3d0e6c25b76cf98b71b528e3d187
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2020-05-22 18:08:42 +0300

    Improvements

--- consumers/alphavantage.py
+++ consumers/alphavantage.py
@@ -4,14 +4,18 @@ import os
 
 def formulate_url(symbol, full=False):
     key = os.getenv("AV_KEY", None)
-    if key is None:
-        return None
+    # Determine the url
+    api_endpoint = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY"
+    size = "full" if full else "compact"
+    interval = str(5)
+    option_url = f"&symbol={symbol}&interval={interval}min&outputsize={size}&datatype=json"
+    key_option = f"apikey={key}"
+    full_url = f"{api_endpoint}{option_url}&{key_option}"
+    return (full_url, key)
 
 def get_raw_data(symbol="MSFT", stamp=0):
-    url = formulate_url(symbol)
-    if url is None:
-        return None
-    
+    url, key = formulate_url(symbol)
+    # Get the data
 
 # Return list of datapoints (named tuples)
 def consume(symbol="MSFT", stamp=None):
@@ -21,3 +25,7 @@ def consume(symbol="MSFT", stamp=None):
     d1.high = 1
     datapoints.append(d1)
     return datapoints
+
+if __name__ == "__main__":
+    url, key = formulate_url("MSFT", False)
+    print(url)
